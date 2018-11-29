ea417d6dcfed8c5a44536c17248b6c69815d4911
Uttaya Jaroenkitwanit <uj@manaosoftware.com>
2018-11-29 15:25:56 +0700

    Added MVC project.

---
+++ orgrammy/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace orgrammy.Controllers
+{
+    public class HomeController : Controller
+    {
+        [RequireHttps]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [RequireHttps]
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        [RequireHttps]
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ orgrammy/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(orgrammy.Startup))]
+namespace orgrammy
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
